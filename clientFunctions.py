86f2c22ccaff61ec64a96182f28d948adc3d8679
phacUFPE <phac@cin.ufpe.br>
2018-12-05 10:40:32 -0300

    Update

--- clientFunctions.py
+++ clientFunctions.py
@@ -1,4 +1,5 @@
 import config as cfg
+from serverFunctions import Server
 import gc
 
 class Client:
@@ -11,20 +12,44 @@ class Client:
         del self.__dest
         gc.collect()
 
+    def client_commands(self, command):
+        command = string.split(" ")[0]
+        arg0 = string.split(" ")[1]
+        arg1 = string.split(" ")[2]
+        if not command or command is None: 
+            return
+        if not arg0 or arg0 is None:
+            return
+        if command == "GET":
+            with open(arg1, "w") as f:
+                while self.__sck.recv(1024) != "COMPLETO!":
+                    fBytes = Server.__decrypt(self.__sck.recv(1024))
+                    f.write(fBytes)
+
+        elif command == "POST":
+            pass
+        elif command == "PUT":
+            pass
+        elif command == "LIST":
+            pass
+        elif command = "DELETE":
+        else:
+            return
+
     def connectToServer(self):
         try:
             self.__sck.connect(self.__dest)
         except ConnectionRefusedError:
             print("Servidor offline!")
-            return            
+            return
         print("Para sair use o  comando 'EXIT'\n")
-        self.__sck.send("sucessfully!".encode())
         print(self.__sck.recv(1024).decode())
-        try:
-            msg = input()
-            while msg != "EXIT":
+        try:            
+            msg = input(":: ")
+            while msg != "EXIT":                
                 self.__sck.send(msg.encode())
-                msg = input()
+                print(self.__sck.recv(1024).decode())
+                msg = input(":: ")
             if msg == "EXIT":
                 self.__sck.send(msg.encode())
         except ConnectionResetError:
--- serverFunctions.py
+++ serverFunctions.py
@@ -1,4 +1,5 @@
 import config as cfg
+import base64
 from threading import Thread
 import _thread as thrd
 import os
@@ -44,64 +45,123 @@ class Server:
             if aut[1] == user and aut[2] == password:
                 if not self.checkDirExist(user): self.createUserDir(user)
                 self.__cliConnecteds[addr] = self.__usersFolder + user
-                print("Usuario {0} logado com sucesso!".format(user))
-                welcomeMSG = "Bem vindo, {0}".format(user)
-                c_sck.send(welcomeMSG.encode())
-                return True
+                print("Usuario {0} logado com sucesso!".format(user))            
+                return [True, user]
             else:
-                c_sck.send("Senha ou login incorretos!".encode())
-        return False
+                return [False, False]
+        return [False, None]
 
-    def __GET(self, fServerPath, clientPath):
+    @staticmethod
+    def __decrypt(enc):
+        key = "IF975 - Projeto de Redes"
+        result = []
+        enc = base64.urlsafe_b64decode(enc).decode()
+        for i in range(len(enc)):
+            key_c = key[i % len(key)]
+            result_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+            result.append(result_c)
+        return "".join(result)
+
+    @staticmethod
+    def __crypt(text):
+        cript = []
+        key = "IF975 - Projeto de Redes"
+        for i in range(len(text)):
+            key_c = key[i % len(key)]
+            cript_c = chr((ord(text[i]) + ord(key_c)) % 256)
+            cript.append(cript_c)
+        return base64.urlsafe_b64encode("".join(cript).encode()).decode()
+
+    def GET(self, fServerPath, clientPath):
+        with open(fServerPath, "rb") as f:
+            fBytes = f.read(1024)
+            fBytes_Crypted = Server.__crypt(fBytes)
+        return fBytes_Crypted
+
+    def POST(self, filePath, fServerPath):
+        with open(filePath, "rb") as f:
+            fileBytes = f.read()
+
+
+    def PUT(self, filePath, fServerPath):
         pass
 
-    def __POST(self, filePath, fServerPath):
+    def DELETE(self, fServerPath):
         pass
 
-    def __PUT(self, filePath, fServerPath):
-        pass
+    def LIST(self, path):
+        return os.listdir(path)
 
-    def __DELETE(self, fServerPath):
-        pass
-
-    def commands(self, string):
+    def commands(self, c_sck, string, dft_dir):
         command = string.split(" ")[0]
         arg0 = string.split(" ")[1]
         arg1 = string.split(" ")[2]
-        if not command or command is None: print("Digite um comando com as especificações!") return
-        if not file or file is None: print("Digite o caminho do arquivo!") return        
+        c_sck.send("{0} - {1} - {2}".format(command, arg0, arg1).encode())
+        if not command or command is None: 
+            c_sck.send("Digite um comando com as especificaï¿½ï¿½es!".encode()) 
+            return
+        if not arg0 or arg0 is None: 
+            c_sck.send("Digite o caminho do arquivo!".encode()) 
+            return        
         if command in self.__commandsDict:
             operation_number = self.__commandsDict[command]
-            if operation_number == 0:                
-                self.__GET(arg1, arg0)
+            if operation_number == 0:
+                f = " "
+                while f != "":
+                    f = self.GET(dft_dir+"/"+arg0, arg1)
+                    c_sck.send(f.encode())
                 #GET (PEGAR ARQUIVO)
             elif operation_number == 1:
-                self.__POST(arg0, arg1)
+                self.POST(arg0, arg1)
                 #POST (COLOCAR ARQUIVO)
             elif operation_number == 2:
-                self.__PUT(arg0, arg1)
+                self.PUT(arg0, arg1)
                 #PUT (SUBSTITUIR ARQUIVO)
+            elif operation_number == 3:
+                self.LIST(dft_dir+arg0)
             else:
-                self.__DELETE(arg0)
+                self.DELETE(arg0)
                 #DELETE (DELETAR ARQUIVO)
         else:
-            print("Isso não é um comando!") return
-
+            c_sck.send("Isso nï¿½o ï¿½ um comando!".encode())
+            return
+ 
     def h_client(self, c_sck, addr):
-        authenticated = False
+        firstTime = True
+        authenticated = [False]
+        user = None
         while True:
-            while not authenticated:
-                c_sck.send("Digite seu usuario e senha\nExemplo: jerome 12345\n\n:: ".encode())
+            while not authenticated[0]:
+                c_sck.send("Digite seu usuario e senha\nExemplo: jerome 12345\n".encode())
                 answ = c_sck.recv(1024).decode()
                 answ = answ.split(" ")
                 if len(answ) > 1:
                     authenticated = self.authentication(c_sck, addr, answ[0], answ[1])
+                    if authenticated[1] is False:
+                        c_sck.send("Senha ou login incorretos!\n".encode())
+                    elif authenticated[1] is None:
+                        c_sck.send("Usuario inexistente!\n".encode())
+                try:
+                    user = authenticated[1]
+                except:
+                    pass
+            if firstTime:
+                firstTime = False
+                c_sck.send("Bem vindo, {0}".format(user).encode())            
+            c_sck.send("""\n
+            \n - COMANDOS
+            \n GET
+            \n POST
+            \n PUT
+            \n LIST
+            \n DELETE\n            
+            """.encode())
             data = c_sck.recv(1024).decode()
+            self.commands(c_sck, data, self.__cliConnecteds[addr])
             if not data: break
             if data == "EXIT": 
                 break
-            print("Cliente: {0}\nMensagem: {1}".format(addr, data))
-            c_sck.send(data.encode())
+            print("Cliente: {0} - Login:{1}\nMensagem: {2}".format(addr, user, data))
         print("Finalizando conexao do Cliente {0}".format(addr))
         self.__cliConnecteds.pop(addr)
         c_sck.close()
